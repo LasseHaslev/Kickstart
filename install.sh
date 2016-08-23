94626781d50104e997f4483982cda959003be076
Lasse S. Haslev <lasse@haslev.no>
2016-08-23 15:34:42 +0200

    Added posibility to rename the folder on setup

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+# Ask for variables to use in this process
+echo "What is your project name?"
+read projectname;
+
 # git clone
-git clone https://github.com/LasseHaslev/Kickstart.git
+git clone https://github.com/LasseHaslev/Kickstart.git $projectname
 
 # Goto folder
-cd kickstart
+cd $projectname
 
 # remove git
 rm -rf .git
