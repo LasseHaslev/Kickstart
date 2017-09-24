ca43256d00dfa0ddbeca1890332c19cdaa3fa749
Lasse <lasse@haslev.no>
2017-09-24 14:25:53 +0200

    Updated install script to setup custom readme

--- install.sh
+++ install.sh
@@ -13,6 +13,19 @@ cd $projectname
 # remove git
 rm -rf .git
 
+# Remove Original Readme
+rm README.md
+
+read projectname;
+# Replace Project name variable to project name
+sed -i '' "s/{{Project}}/$projectname/g" KICKSTARTED_README.md
+
+# Move Kickstarted readme in place
+mv KICKSTARTED_README.md README.md
+
+# Remove install script
+rm install.sh
+
 # install dependencies
 npm install
 
